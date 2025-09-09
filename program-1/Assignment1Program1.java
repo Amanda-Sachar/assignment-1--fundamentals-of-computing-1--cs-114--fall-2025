c552432fab91459ff89596a6728929f8f76fb49e
amanda-sachar <mandysachar@gmail.com>
2025-09-09 15:24:07 -0400

    made initials
    I wrote down my initials first on graph paper to determine where I should put spaces and where I should put a letter. Then I wrote code to tell the program to write out each line.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,9 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
+System.out.println(" AA   SSS");
+System.out.println("A  A S  ");
+System.out.println("A  A  SSS");
+System.out.println("AAAA     S");
+System.out.println("A  A  SSS ");
   }
 }
