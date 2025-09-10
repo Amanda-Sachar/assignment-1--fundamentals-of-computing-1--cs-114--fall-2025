6a781e9d481ff2f2be2f7b79f59104242984a9fa
amanda-sachar <mandysachar@gmail.com>
2025-09-10 19:10:56 -0400

    Added Scanner
    I added a scanner to get the input, and to make sure it works I had it print what the input was.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
 System.out.println(" AA   SSS");
@@ -5,5 +7,11 @@ System.out.println("A  A S  ");
 System.out.println("A  A  SSS");
 System.out.println("AAAA     S");
 System.out.println("A  A  SSS ");
-  }
+String message;
+Scanner scan = new Scanner(System.in);
+System.out.println("enter a 5 letter string:");
+message=scan.nextLine();
+System.out.println("you entered: \"" + message+ "\"");
+scan.close();
+}
 }
